2692d5b8de896900798ae6e4037f82c3619e7746
Shin-NiL <niltonr@gmail.com>
2016-04-25 21:21:46 -0300

    Example code fixed

--- example/admob_manager.gd
+++ example/admob_manager.gd
@@ -4,7 +4,7 @@ extends Button
 var admob = null
 #get this string when you register your app in adbmob backend
 #If you do not put the correct app publisher id, you won't get any ads
-var admob_app_id = "ca-app-pub-xxxxxxxxxxxxxxxxxxxxxxxxxxx" 
+var admob_app_id = "ca-app-pub-3940256099942544/6300978111" 
 
 func _init():
 	if(Globals.has_singleton("bbAdmob")):
@@ -12,8 +12,7 @@ func _init():
 		#You can call admob.init_admob_test or admob.init_admob_real
 		#If the second argument is true, the banner ad will be at the top of the screen
 		#Function prototype init_admob_test(final String app_id, boolean isTop)
-		admob.init_admob_test(get_instance_ID(), admob_app_id, true)
-#	connect("pressed", self, "_on_Show_Banner_Button_pressed")
+		admob.init_admob_banner_test(get_instance_ID(), admob_app_id, false)
 	
 func _ready():
 	# Initialization here
@@ -22,13 +21,10 @@ func _ready():
 
 
 func _on_Show_Banner_Button_pressed():
-	admob.show_banner()
+	if admob != null:
+		admob.show_banner()
 	
 	
 func _on_Hide_Banner_Button_pressed():
-	admob.hide_banner()
-
-
-func _on_Show_Interstitial_Button_pressed():
-	admob.show_interstitial()
-
+	if admob != null:
+		admob.hide_banner()
